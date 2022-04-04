6aef3b8d9de648f2e469089ad35836fc8c8ee359
Elikul <linotska@inbox.ru>
2022-04-04 19:38:17 +0300

    выделение вычесленй из действий

--- RSA.py
+++ RSA.py
@@ -40,6 +40,53 @@ def chooseE(phi): #явный вход
         if (gcd(e, phi) == 1):
             return e #явный выход
 
+
+"""
+Прочитать из файла данные
+"""
+#вычисление/#действие
+def readFromFile(file_name):  #явный вход
+    data = []
+
+    try:
+        file = open(file_name, 'r')
+    except FileNotFoundError:
+        print('Файл c таким именем не найден')
+    else:
+        data = file.read().splitlines()
+        file.close()
+    return data  #явный выход
+
+"""
+Записать в файл ключи
+"""
+#действие
+def writeKeysToFile(file_name, a, b): #явный вход
+    f_public = open(file_name, 'w')
+    f_public.write(str(a) + '\n')
+    f_public.write(str(b) + '\n')
+    f_public.close()
+
+"""
+Вычислить n, phi(n), e
+"""
+#действие
+def getParams(prime1, prime2): #явный вход
+    n = prime1 * prime2
+    phi = (prime1 - 1) * (prime2 - 1)
+    e = chooseE(phi) #побочное действие
+    return n, phi, e  #явный выход
+
+"""
+Получить часть закрытого ключа
+"""
+#вычисление
+def getD(coaf_a, phi): #явный вход
+    if (coaf_a < 0):
+        return coaf_a + phi  #явный выход
+    else:
+        return coaf_a  #явный выход
+
 """
 Выбирает два случайных простых числа из списка простых чисел (значения, доходящие до 100 тыс.)
 Используя простые числа,вычисляет и хранит открытый и закрытый ключи в двух отдельных
@@ -53,9 +100,7 @@ def chooseKeys():
     rand2 = random.randint(100, 300) #неявный вход
 
     # открываем txt-файл простых чисел и получаем содержимое
-    prime_file = open('primes-100k.txt', 'r')
-    numbers = prime_file.read().splitlines()
-    prime_file.close()
+    numbers = readFromFile('primes-100k.txt')
 
     # выбрнные простые числа
     prime1 = int(numbers[rand1]) #неявный вход
@@ -63,30 +108,47 @@ def chooseKeys():
 
     
     # вычисляем n, phi(n), e
-    n = prime1 * prime2
-    phi = (prime1 - 1) * (prime2 - 1)
-    e = chooseE(phi)
+    n, phi, e = getParams(prime1, prime2) #неявный вход
 
     # вычисляем d, 1 < d < phi(n) так, что e*d = 1 (mod phi(n))
     # e и d - обратные (mod phi(n))
     gcd, a, b = xgcd(e, phi) #неявный вход
    
     # убедимся, что d положительный
-    if (a < 0):
-        d = a + phi
-    else:
-        d = a
+    d = getD(a, phi) #неявный вход
 
-    # записываем открытый ключи (n,e) в файл
-    f_public = open('public_keys.txt', 'w')
-    f_public.write(str(n) + '\n')
-    f_public.write(str(e) + '\n')
-    f_public.close()
+    # записываем открытый ключ (n,e) в файл
+    writeKeysToFile('public_keys.txt', n, e)
 
-    f_private = open('private_keys.txt', 'w')
-    f_private.write(str(n) + '\n')
-    f_private.write(str(d) + '\n')
-    f_private.close()
+    # записываем закрытый ключ (n,d) в файл
+    writeKeysToFile('private_keys.txt', n, d)
+
+
+"""
+Разделить сообщение на блоки
+"""
+#вычисление
+def divideMsgOnBlocks(message, block_size): #явный вход
+    encrypted_blocks = []
+    ciphertext = -1
+
+    if (len(message) > 0):    
+        # инициализировать зашифрованный текст ASCII первого символа сообщения
+        ciphertext = ord(message[0])
+
+    for i in range(1, len(message)):
+        # добавить зашифрованный текст в список, если достигнут максимальный размер блока
+        # сбросить зашифрованный текст, чтобы мы могли продолжить добавление кодов ASCII
+        if (i % block_size == 0):
+            encrypted_blocks.append(ciphertext)
+            ciphertext = 0
+
+        # умножем на 10000, чтобы сдвинуть цифры влево на 4 разряда
+        # потому что коды ASCII состоят максимум из 4-х десятичных цифр
+        ciphertext = ciphertext * 10000 + ord(message[i]) #неявный выход
+    # добавляем последний блок в список
+    encrypted_blocks.append(ciphertext)
+    return encrypted_blocks
 
 
 """
@@ -98,71 +160,45 @@ file_name - файл c отрытым. Если файла нет
 block_size указывает, сколько символов составляют одну группу чисел в
 каждый индекс encrypted_blocks.
 """
-#вычисление
+#вычисление/#действие
 def encrypt(message, file_name = 'public_keys.txt', block_size = 1): #явный вход
-    
-    try:
-        f_public = open(file_name, 'r')
 
-    # проверяем возможность того, что пользователь пытается что-то зашифровать
-    # используя открытый ключ, который не найден
-    except FileNotFoundError:
-        print('Файл c таким именем не найден')
-    else:
-        n = int(f_public.readline()) #неявный вход
-        e = int(f_public.readline()) #неявный вход
-        f_public.close()
+    public_key = readFromFile(file_name) #вычисление/#действие
+    encrypted_blocks = divideMsgOnBlocks(message, block_size) #вычисление
 
-        encrypted_blocks = []
-        ciphertext = -1
+    # m^e mod n
+    for i in range(len(encrypted_blocks)):
+        encrypted_blocks[i] = str((encrypted_blocks[i]**int(public_key[1])) % int(public_key[0]))
 
-        if (len(message) > 0):
-            
-            # инициализировать зашифрованный текст ASCII первого символа сообщения
-            ciphertext = ord(message[0])
+    # создаем строку из чисел
+    encrypted_message = " ".join(encrypted_blocks) #неявный выход
 
-        for i in range(1, len(message)):
-            # добавить зашифрованный текст в список, если достигнут максимальный размер блока
-            # сбросить зашифрованный текст, чтобы мы могли продолжить добавление кодов ASCII
-            if (i % block_size == 0):
-                encrypted_blocks.append(ciphertext)
-                ciphertext = 0
+    return encrypted_message #явный выход
 
-            # умножем на 10000, чтобы сдвинуть цифры влево на 4 разряда
-            # потому что коды ASCII состоят максимум из 4-х десятичных цифр
-            ciphertext = ciphertext * 10000 + ord(message[i]) #неявный выход
+"""
+Представляет сообщение как список целых
+"""
+#вычисление
+def msgInIntBlocks(blocks): #явный вход
+   # превращает строку в список целых чисел
+    list_blocks = blocks.split(' ')  #неявный вход
+    int_blocks = []
 
-        # добавляем последний блок в список
-        encrypted_blocks.append(ciphertext)
-
-        # m^e mod n
-        for i in range(len(encrypted_blocks)):
-            encrypted_blocks[i] = str((encrypted_blocks[i]**e) % n)
-
-        # создаем строку из чисел
-        encrypted_message = " ".join(encrypted_blocks) #неявный выход
-
-        return encrypted_message #явный выход
+    for s in list_blocks:
+        int_blocks.append(int(s)) 
+    return int_blocks #явный выход
 
 """
 Расшифровывает строку чисел c^d mod n. Возвращает сообщение в виде строки.
 block_size указывает, сколько символов составляют одну группу чисел в
 каждый индекс блоков.
 """
-#вычисление
+#вычисление/#действие
 def decrypt(blocks, file_name = 'private_keys.txt', block_size = 1): #явный вход
 
-    f_private = open(file_name, 'r')
-    n = int(f_private.readline()) #неявный вход
-    d = int(f_private.readline()) #неявный вход
-    f_private.close()
+    private_key = readFromFile(file_name) #вычисление/#действие
 
-    # превращает строку в список целых чисел
-    list_blocks = blocks.split(' ')  #неявный вход
-    int_blocks = []
-
-    for s in list_blocks:
-        int_blocks.append(int(s))
+    int_blocks = msgInIntBlocks(blocks) #вычисление
 
     message = ""
 
@@ -170,7 +206,7 @@ def decrypt(blocks, file_name = 'private_keys.txt', block_size = 1): #явный
     # по умолчанию каждый int представляет два символа
     for i in range(len(int_blocks)):
         # расшифровать все числа c^d mod n
-        int_blocks[i] = (int_blocks[i]**d) % n
+        int_blocks[i] = (int_blocks[i]**int(private_key[1])) % int(private_key[0])
         
         tmp = ""
        
